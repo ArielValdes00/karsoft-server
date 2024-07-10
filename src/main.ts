813e749e7bcb8192b2b6a4ee6abc6bed240a885e
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2024-07-10 15:14:21 -0300

    added variable PORT

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
         origin: [process.env.FRONTEND_URL], 
         credentials: true,
     });
-    await app.listen(4000);
+    await app.listen(process.env.PORT || 4000);
 }
 bootstrap();

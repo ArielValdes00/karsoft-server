077fcea6d2c492af68c1d0da329a1139b343295c
ArielValdes00 <124757872+ArielValdes00@users.noreply.github.com>
2024-07-18 23:05:08 -0300

    update

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,20 +1,25 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
+export class UpdateUserDto {
+    @IsNotEmpty({ message: 'El nombre es obligatorio' })
+    name: string;
+
     @IsEmail({}, { message: 'El correo electrónico no es válido' })
     email?: string;
 
-    @IsNotEmpty({ message: 'El número de teléfono es obligatorio' })
+    @IsNotEmpty({ message: 'El Telefono es obligatorio' })
+    @IsOptional()
     phone_number?: string;
 
-    @IsNotEmpty({ message: 'El nombre del negocio es obligatorio' })
+    @IsNotEmpty({ message: 'El Local es obligatorio' })
+    @IsOptional()
     business_name?: string;
 
-    @IsNotEmpty({ message: 'La dirección es obligatoria' })
+    @IsNotEmpty({ message: 'La dirección es obligatorio' })
+    @IsOptional()
     address?: string;
 
     @IsNotEmpty({ message: 'El código postal es obligatorio' })
+    @IsOptional()
     postal_code?: string;
 }
